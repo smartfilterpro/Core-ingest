cb302ea1937d0ec086209fb7bfd9f463d05185e3
smartfilterpro <eric@smartfilterpro.com>
2025-10-10 11:44:05 -0400

    Update index.ts

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -2,6 +2,5 @@
 export { runSessionStitcher } from './sessionStitcher';
 export { runSummaryWorker } from './summaryWorker';
 export { runRegionAggregationWorker } from './regionAggregationWorker';
-export { runAIWorker } from './aiWorker';
 export { bubbleSummarySync } from './bubbleSummarySync';
 export { heartbeatWorker } from './heartbeatWorker';
